bcab0a68ec03cd45e02ef95da4f3ee419ded8a92
Rakpong Kittinaradorn <rkcosmos@gmail.com>
2020-04-23 16:38:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     description='End-to-End Multi-Lingual Optical Character Recognition (OCR) Solution',
     author='Rakpong Kittinaradorn',
     author_email='r.kittinaradorn@gmail.com',
-    url='https://github.com/jaidedai/easy_ocr',
-    download_url='https://github.com/jaidedai/easy_ocr.git',
+    url='https://github.com/jaidedai/easyocr',
+    download_url='https://github.com/jaidedai/easyocr.git',
     keywords=['ocr optical character recognition deep learning neural network'],
     classifiers=[
         'Development Status :: 5 - Production/Stable'
