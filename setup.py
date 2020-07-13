5f37b78578140b98a1a9480b6c57030611851480
Deepraj <bdeeprajrkm1@gmail.com>
2020-07-13 21:35:28 +0530

    Modified setup.py for ease of installation with requirements

--- setup.py
+++ setup.py
@@ -4,15 +4,25 @@ End-to-End Multi-Lingual Optical Character Recognition (OCR) Solution
 
 from setuptools import setup
 
+with open('requirements.txt') as f: 
+	requirements = f.readlines() 
+
+def readme():
+	with open('README.md') as f:
+		README = f.read()
+	return README
+
 setup(
     name='easyocr',
     packages=['easyocr'],
     include_package_data=True,
     version='1.1.4',
-    install_requires=['torch', 'torchvision>=0.5', 'opencv-python', 'scipy', 'numpy', 'Pillow', 'scikit-image'],
+    install_requires = requirements, 
     entry_points={"console_scripts": ["easyocr= easyocr.cli:main"]},
     license='Apache License 2.0',
     description='End-to-End Multi-Lingual Optical Character Recognition (OCR) Solution',
+    long_description = readme(), 
+	long_description_content_type ="text/markdown", 
     author='Rakpong Kittinaradorn',
     author_email='r.kittinaradorn@gmail.com',
     url='https://github.com/jaidedai/easyocr',
