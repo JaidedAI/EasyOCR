d94d1f31d228e46d72c784ce083bada1e2d6e577
Rakpong Kittinaradorn <rkcosmos@gmail.com>
2020-07-14 06:55:42 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ End-to-End Multi-Lingual Optical Character Recognition (OCR) Solution
 
 from setuptools import setup
 
-with open('requirements.txt') as f: 
-	requirements = f.readlines() 
+with open('requirements.txt', encoding = "utf-8-sig") as f: 
+    requirements = f.readlines() 
 
 def readme():
-	with open('README.md') as f:
-		README = f.read()
-	return README
+    with open('README.md', encoding = "utf-8-sig") as f:
+	README = f.read()
+    return README
 
 setup(
     name='easyocr',
@@ -21,8 +21,8 @@ setup(
     entry_points={"console_scripts": ["easyocr= easyocr.cli:main"]},
     license='Apache License 2.0',
     description='End-to-End Multi-Lingual Optical Character Recognition (OCR) Solution',
-    long_description = readme(), 
-	long_description_content_type ="text/markdown", 
+    long_description = readme(),
+    long_description_content_type ="text/markdown", 
     author='Rakpong Kittinaradorn',
     author_email='r.kittinaradorn@gmail.com',
     url='https://github.com/jaidedai/easyocr',
