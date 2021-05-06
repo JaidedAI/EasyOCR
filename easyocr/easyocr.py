3d0012d5932dccd1a1dc459322bbd1011ff09802
rkcosmos <rkcosmos@gmail.com>
2021-05-07 04:57:29 +0700

    suppress cuda warning

--- easyocr/easyocr.py
+++ easyocr/easyocr.py
@@ -64,10 +64,12 @@ class Reader(object):
 
         if gpu is False:
             self.device = 'cpu'
-            LOGGER.warning('Using CPU. Note: This module is much faster with a GPU.')
+            if verbose:
+                LOGGER.warning('Using CPU. Note: This module is much faster with a GPU.')
         elif not torch.cuda.is_available():
             self.device = 'cpu'
-            LOGGER.warning('CUDA not available - defaulting to CPU. Note: This module is much faster with a GPU.')
+            if verbose:
+                LOGGER.warning('CUDA not available - defaulting to CPU. Note: This module is much faster with a GPU.')
         elif gpu is True:
             self.device = 'cuda'
         else:
