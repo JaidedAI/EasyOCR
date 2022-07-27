f284b998f56f4fc31b10f153b8a3dcc16ced738d
gianni <gianni.galbiati@vidrovr.com>
2022-07-27 11:34:32 -0400

    torchscript can't serialize AdaptiveAvgPool2d with `None` as a target size, but this is equivalent to a `torch.mean` call.

--- easyocr/model/vgg_model.py
+++ easyocr/model/vgg_model.py
@@ -1,35 +1,27 @@
+import torch
 import torch.nn as nn
 from .modules import VGG_FeatureExtractor, BidirectionalLSTM
 
-class Model(nn.Module):
 
+class Model(nn.Module):
     def __init__(self, input_channel, output_channel, hidden_size, num_class):
         super(Model, self).__init__()
-        """ FeatureExtraction """
         self.FeatureExtraction = VGG_FeatureExtractor(input_channel, output_channel)
         self.FeatureExtraction_output = output_channel
-        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))
 
-        """ Sequence modeling"""
         self.SequenceModeling = nn.Sequential(
             BidirectionalLSTM(self.FeatureExtraction_output, hidden_size, hidden_size),
             BidirectionalLSTM(hidden_size, hidden_size, hidden_size))
         self.SequenceModeling_output = hidden_size
 
-        """ Prediction """
         self.Prediction = nn.Linear(self.SequenceModeling_output, num_class)
 
-
     def forward(self, input, text):
-        """ Feature extraction stage """
         visual_feature = self.FeatureExtraction(input)
-        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))
-        visual_feature = visual_feature.squeeze(3)
+        visual_feature = torch.mean(visual_feature.permute(0, 3, 1, 2), dim=3)
 
-        """ Sequence modeling stage """
         contextual_feature = self.SequenceModeling(visual_feature)
 
-        """ Prediction stage """
         prediction = self.Prediction(contextual_feature.contiguous())
 
         return prediction
