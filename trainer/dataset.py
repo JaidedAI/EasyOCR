d6c066b7a2b9df81a6a91afaca86285605085d92
shuheilocale <default@default.com>
2022-08-24 18:20:43 +0900

    Update csv parser

--- trainer/dataset.py
+++ trainer/dataset.py
@@ -146,7 +146,7 @@ class OCRDataset(Dataset):
         self.root = root
         self.opt = opt
         print(root)
-        self.df = pd.read_csv(os.path.join(root,'labels.csv'), sep='^([^,]+),', engine='python', usecols=['filename', 'words'], keep_default_na=False)
+        self.df = pd.read_csv(os.path.join(root,'labels.csv'), engine='python', usecols=['filename', 'words'], keep_default_na=False)
         self.nSamples = len(self.df)
 
         if self.opt.data_filtering_off:
