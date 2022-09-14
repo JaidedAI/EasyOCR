4377b344632509f2c56b483433ca27496cdcb84d
JulianOrteil <dakota.horstman2001@gmail.com>
2022-09-13 22:26:11 -0500

    Automate getting version of library from git tags

--- easyocr/__init__.py
+++ easyocr/__init__.py
@@ -1,3 +1,16 @@
-from .easyocr import Reader
+import sys
 
-__version__ = '1.6.1'
+if sys.version_info[:2] >= (3, 8):
+    from importlib.metadata import PackageNotFoundError, version
+else:
+    from importlib_metadata import PackageNotFoundError, version
+
+dist_name = "easyocr"
+try:
+    __version__ = version(dist_name)
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+from .easyocr import Reader
