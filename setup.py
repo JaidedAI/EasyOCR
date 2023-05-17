51994639dda8cfd1ffbaf3862308544129e480bc
Samuel Yvon <samuelyvon9@gmail.com>
2023-05-17 16:47:03 -0400

    Poetry script seems to work

--- setup.py
+++ setup.py
@@ -13,20 +13,20 @@ def readme():
     return README
 
 setup(
-    name='easyocr',
-    packages=['easyocr'],
+    # name='easyocr',
+    # packages=['easyocr'],
     include_package_data=True,
-    version='1.7.0',
-    install_requires=requirements,
-    entry_points={"console_scripts": ["easyocr= easyocr.cli:main"]},
-    license='Apache License 2.0',
-    description='End-to-End Multi-Lingual Optical Character Recognition (OCR) Solution',
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    author='Rakpong Kittinaradorn',
-    author_email='r.kittinaradorn@gmail.com',
-    url='https://github.com/jaidedai/easyocr',
-    download_url='https://github.com/jaidedai/easyocr.git',
+    # version='1.6.2',
+    # install_requires=requirements,
+    # entry_points={"console_scripts": ["easyocr= easyocr.cli:main"]},
+    # license='Apache License 2.0',
+    # description='End-to-End Multi-Lingual Optical Character Recognition (OCR) Solution',
+    # long_description=readme(),
+    # long_description_content_type="text/markdown",
+    # author='Rakpong Kittinaradorn',
+    # author_email='r.kittinaradorn@gmail.com',
+    # url='https://github.com/jaidedai/easyocr',
+    # download_url='https://github.com/jaidedai/easyocr.git',
     keywords=['ocr optical character recognition deep learning neural network'],
     classifiers=[
         'Development Status :: 5 - Production/Stable'
