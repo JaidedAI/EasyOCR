5e10bd44c1e966962e41235b08965ab48f723fe9
0x726f6f6b6965 <h123453047@icloud.com>
2025-08-04 02:29:35 +0800

    rename trainer/test.py for `ImportError`

--- trainer/train.py
+++ trainer/train.py
@@ -14,7 +14,7 @@ import numpy as np
 from utils import CTCLabelConverter, AttnLabelConverter, Averager
 from dataset import hierarchical_dataset, AlignCollate, Batch_Balanced_Dataset
 from model import Model
-from test import validation
+from validate import validation
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def count_parameters(model):
